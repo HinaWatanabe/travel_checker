72bad823f7a63b2ea581f88fec1f4fe3972ff35f
HinaWatanabe <watanabehina.0806@gmail.com>
2026-06-08 02:24:06 +0900

    おすすめスポットの追加

--- spots.js
+++ spots.js
@@ -1,6 +1,8 @@
 const frogSpots = [
     {pref: "和歌山県", name: "かえる橋", address: "和歌山県日高郡印南町印南"},
     {pref: "静岡県", name: "Kawazoo", address: "静岡県和歌山県日高郡印南町印南"},
-    {pref: "岐阜県", name: "下呂温泉", address: "岐阜県下呂市"}
+    {pref: "岐阜県", name: "下呂温泉", address: "岐阜県下呂市"},
+    {pref: "岐阜県", name: "アクア・トトぎふ", address: "岐阜県各務原市川島笠田町1453"},
+    {pref: "長野県", name: "縄手通り", address: "長野県松本市大手3丁目3-1"}
     
   ];
